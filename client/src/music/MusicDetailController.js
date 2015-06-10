cb8c34518a0752fa1dbf2f7091481f17d532e8ce
Piotr Lewandowski <grovman@gmail.com>
2015-06-10 10:31:50 +0200

    Shows productivity in procentage bars

--- client/src/music/MusicDetailController.js
+++ client/src/music/MusicDetailController.js
@@ -7,10 +7,35 @@
 
     function MusicDetailController($mdBottomSheet, track) {
         var vm = this;
+
         vm.track = track;
-        console.log(track);
+        var productivity = track.productivity;
+        var maxValue = Math.max(productivity.productive, productivity.neutral, productivity.unproductive);
+
+        vm.productivity = [
+            {
+                name: 'productive',
+                procentage: Math.floor(productivity.productive / maxValue),
+                minutes: toSpentMinutes(productivity.productive)
+            },
+            {
+                name: 'neutral',
+                procentage: Math.floor(productivity.neutral / maxValue),
+                minutes: toSpentMinutes(productivity.neutral)
+            },
+            {
+                name: 'unproductive',
+                procentage: Math.floor(productivity.unproductive / maxValue),
+                minutes: toSpentMinutes(productivity.unproductive)
+            }];
+
+        console.log(vm.productivity);
 
         this.hide = $mdBottomSheet.hide;
+    }
+
+    function toSpentMinutes(seconds) {
+        return Math.floor(seconds / 60);
     }
 
 
