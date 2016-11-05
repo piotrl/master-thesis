6f65f1e352d25164a1c1a13ab81a7b41b5411e79
Piotr Lewandowski <grovman@gmail.com>
2016-11-05 14:16:50 +0100

    #7 Prepare Spring boot project

---
+++ project/src/main/java/net/piotrl/music/MusicApplication.java
@@ -0,0 +1,12 @@
+package net.piotrl.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+}
