f0a64fb8c2f146badf4b1ffc2a4bcaee1802c392
Piotr Lewandowski <grovman@gmail.com>
2016-11-20 22:34:00 +0100

    #7 Add Lombok project - to remove boilerplate

--- project/src/main/java/net/piotrl/music/aggregation/lastfm/LastFmAuthProperties.java
+++ project/src/main/java/net/piotrl/music/aggregation/lastfm/LastFmAuthProperties.java
@@ -1,37 +1,10 @@
 package net.piotrl.music.aggregation.lastfm;
 
+import lombok.Data;
+
+@Data
 public class LastFmAuthProperties {
-    private String api_key;
-    private String secret;
-    private String username;
-
-    public LastFmAuthProperties(String api_key, String secret, String username) {
-        this.api_key = api_key;
-        this.secret = secret;
-        this.username = username;
-    }
-
-    public String getApi_key() {
-        return api_key;
-    }
-
-    public void setApi_key(String api_key) {
-        this.api_key = api_key;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    private final String api_key;
+    private final String secret;
+    private final String username;
 }
