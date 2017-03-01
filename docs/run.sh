98908e410c4ff8048cf85f9ba2ec01e143cd0b58
Piotr Lewandowski <grovman@gmail.com>
2017-03-01 21:40:15 +0100

    Fix URLs length + add two images in introduction
    Signed-off-by: Piotr Lewandowski <grovman@gmail.com>

--- docs/run.sh
+++ docs/run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-xelatex ./magisterka.tex && start ./magisterka.pdf
+#COMPILE=$(xelatex magisterka)
+#BIBLIOGRAPHY=$(bibtex magisterka)
+
+rm -rf *.{log,aux,out,lot,lof,ilg,toc,blg,synctex.gz} *~
+
+xelatex magisterka --no-pdf && xelatex magisterka && start ./magisterka.pdf
