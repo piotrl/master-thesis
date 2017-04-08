662d022964d4ee3f66f86d8b881d251bf2d057fe
Piotr Lewandowski <grovman@gmail.com>
2017-04-09 00:25:43 +0200

    Add first google chart as example
    Signed-off-by: Piotr Lewandowski <grovman@gmail.com>

---
+++ project/src/main/resources/static/reports/reports-music-ratio.js
@@ -0,0 +1,36 @@
+(function (reports) {
+    console.log(reports.filterDate);
+    // this month
+    google.charts.load('current', {'packages': ['corechart']});
+
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        console.log("drawChart");
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+            ['Mushrooms', 3],
+            ['Onions', 1],
+            ['Olives', 1],
+            ['Zucchini', 1],
+            ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {
+            'title': 'How Much Pizza I Ate Last Night',
+            'width': 400,
+            'height': 300
+        };
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('reports-music-ratio'));
+        chart.draw(data, options);
+    }
+
+})(window.app.reports);
---
+++ project/src/main/resources/static/reports/reports.service.js
@@ -0,0 +1,5 @@
+(function (app) {
+    app.reports = {
+        filterDate: new Date()
+    };
+})(window.app || {});
