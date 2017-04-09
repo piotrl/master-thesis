5acb454b4544b48d4ec2c14b162a54b384ac287b
Piotr Lewandowski <grovman@gmail.com>
2017-04-09 11:58:28 +0200

    Autofill filter date with current month
    - Add date-fns library
    
    Signed-off-by: Piotr Lewandowski <grovman@gmail.com>

--- project/src/main/resources/static/reports/reports.service.js
+++ project/src/main/resources/static/reports/reports.service.js
@@ -2,4 +2,7 @@
     app.reports = {
         filterDate: new Date()
     };
+
+    const $input = document.getElementById("filter-month");
+    $input.value = dateFns.format(app.reports.filterDate, 'YYYY-MM');
 })(window.app || {});
