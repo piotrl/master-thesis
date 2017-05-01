1e70ebfc604fb176d84f4d29202c2b1803f13b58
Piotr Lewandowski <grovman@gmail.com>
2017-05-01 17:43:22 +0200

    feat(bibliography): Configure bibliography with first entry

--- docs/run.sh
+++ docs/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-#COMPILE=$(xelatex magisterka)
-#BIBLIOGRAPHY=$(bibtex magisterka)
 
 rm -rf *.{log,aux,out,lot,lof,ilg,toc,blg,synctex.gz} *~
 
-xelatex magisterka --no-pdf && xelatex magisterka && start ./magisterka.pdf
+xelatex magisterka --no-pdf && bibtex magisterka && xelatex magisterka && start ./magisterka.pdf
