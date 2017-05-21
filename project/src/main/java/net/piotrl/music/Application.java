2d0262659002c89be0f47603eff52bf0a3c42482
Piotr Lewandowski <grovman@gmail.com>
2017-05-21 21:52:31 +0200

    chore(build) Tweaking build process to make it work on Tomcat

--- project/src/main/java/net/piotrl/music/Application.java
+++ project/src/main/java/net/piotrl/music/Application.java
@@ -2,17 +2,24 @@ package net.piotrl.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	@Bean
